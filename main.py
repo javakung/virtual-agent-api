79a5431e6c7bcfbac566791cc095d546ac7d63b0
Chatchawal Sangkeettrakarn <conan@conan.in.th>
2020-09-19 16:25:36 +0700

    change port 5000 to 80

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def read_root():
     return {"Hello": "World"}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=5000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=80, debug=True)
