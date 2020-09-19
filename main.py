eb9def424e791ac75665fa548c46db3962009201
Chatchawal Sangkeettrakarn <conan@conan.in.th>
2020-09-19 16:23:06 +0700

    add root function

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/")
-async def main():
-    return 'Deploy Model Tutorial'
+def read_root():
+    return {"Hello": "World"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=5000, debug=True)
