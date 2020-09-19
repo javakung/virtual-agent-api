d6b6fe03dfce45a89b89b77f1f0e73990d5edaad
Chatchawal Sangkeettrakarn <conan@conan.in.th>
2020-09-19 17:05:19 +0700

    change port to 80

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def main():
 
 if __name__ == '__main__':
    import uvicorn
-   uvicorn.run(app, host="0.0.0.0", port=5000, debug=True) 
+   uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
